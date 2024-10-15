a67349888640e57fd875abe8a749bea03763f4c1
Jovana667 <jovanavukovic4@hotmail.com>
2024-10-15 18:11:05 +1100

    comits

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("../client/dist"));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/htmlRoutes.js")(app);
+require("./routes/htmlRoutes")(app);
 
-app.listen(PORT, function () {
-  console.log(`Now listening on port: ${PORT}`);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
