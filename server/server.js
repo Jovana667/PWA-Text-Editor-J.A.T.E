fca28bb37303d35abd4a15d1c9e2ea6f2943dcb1
Jovana667 <jovanavukovic4@hotmail.com>
2024-10-15 18:05:53 +1100

    updates

--- server/server.js
+++ server/server.js
@@ -1,17 +1,14 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static("../client/dist"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/htmlRoutes")(app);
+require("./routes/htmlRoutes.js")(app);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+app.listen(PORT, function () {
+  console.log(`Now listening on port: ${PORT}`);
 });
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
